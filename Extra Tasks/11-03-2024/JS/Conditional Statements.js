e919f39b4d6bcc5b992d160e6d38257f8c2750da
Harishmmdl1 <harishmmdl@gmail.com>
2024-03-11 17:53:59 +0530

    condiotional statement practice

---
+++ Extra Tasks/11-03-2024/JS/Conditional Statements.js
@@ -0,0 +1,35 @@
+// Conditional Statements in JS
+
+// 8 Questions to practice
+
+// 1. WAJSP to tell the wwather status as per user entered value
+
+// num greater and qual to 30 : - Hot (Summer)
+// num between 18 - 29 :- Nuetral
+// num less than 18 : Cold
+
+// let a = Number(prompt("Enter the Temperature in your Surrounding : - "));
+
+// if(a<=18)
+// {
+//     console.log("Your in Cold Temperature");
+// }
+// else if(a>18 && a<=29)
+// {
+//     console.log("Your in Nuetral Temperature");
+// }
+// else
+// {
+//     console.log("Your in Hot (summer) Temperature");
+// }
+
+// 2. WAJSP to check weather the year is leap year or not
+
+// let year = Number(prompt("Enter the year to check Leap Year or not: - "));
+
+// if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+//     console.log(year + " is a leap year.");
+// } else {
+//     console.log(year + " is not leap year.");
+// }
+
